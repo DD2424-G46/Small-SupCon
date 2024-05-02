0daf9f2d22aeecef226583b89fcbd36f0a050c9f
Martin Lindefors <lindefor@kth.se>
2024-05-02 13:27:27 +0200

    Base model implemented
    Co-authored-by: VendelaAsplund <Vendelala@users.noreply.github.com>

---
+++ main.py
@@ -0,0 +1,75 @@
+import sys
+from matplotlib import pyplot as plt
+from keras.datasets import cifar10
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.optimizers import SGD
+
+
+def load_dataset():
+    (trainX, trainY), (testX, testY) = cifar10.load_data()
+
+    # One-hot-encoding
+    trainY = to_categorical(trainY)
+    testY = to_categorical(testY)
+    return trainX, trainY, testX, testY
+
+def prep_pixels(train, test):
+    train_norm = train.astype('float32')
+    test_norm = test.astype('float32')
+
+    train_norm = train_norm / 255.0
+    test_norm = test_norm / 255.0
+
+    return train_norm, test_norm
+
+def define_model():
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(32, 32, 3)))
+    print(model.output_shape)
+    model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+    print(model.output_shape)
+    model.add(MaxPooling2D((2,2)))
+    print(model.output_shape)
+    model.add(Flatten())
+    print(model.output_shape)
+    model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+    print(model.output_shape)
+    model.add(Dense(10, activation='softmax'))
+    print(model.output_shape)
+
+    opt = SGD(learning_rate=0.001, momentum=0.9)
+    model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+def summarize_diagnostics(history):
+    plt.subplot(211)
+    plt.title('Cross Entropy Loss')
+    plt.plot(history.history['loss'], color='blue', label='test')
+    plt.plot(history.history['val_loss'], color='orange', label='test')
+
+    plt.subplot(212)
+    plt.title('Classification Accuracy')
+    plt.plot(history.history['accuracy'], color='blue', label='train')
+    plt.plot(history.history['val_accuracy'], color='orange', label='test')
+
+    
+    filename = sys.argv[0].split('/')[-1]
+    plt.savefig(filename + '_plot.png')
+    plt.close()
+
+def run_test_harness():
+    trainX, trainY, testX, testY = load_dataset()
+    trainX, testX = prep_pixels(trainX, testX)
+    model = define_model()
+    history = model.fit(trainX, trainY, epochs=100, batch_size=64, validation_data=(testX, testY), verbose=0)
+    _, acc = model.evaluate(testX, testY, verbose=0)
+    print('> %.3f' % (acc * 100.0))
+    summarize_diagnostics(history)
+
+
+run_test_harness()
